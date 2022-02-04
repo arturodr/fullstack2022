efc0086567b243bc28fc12a2ac27988e327029bf
R2 <arturodr@gmail.com>
2022-02-04 11:48:09 -0500

    replace with real venues data.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -104,29 +104,14 @@ def index():
 
 @app.route('/venues')
 def venues():
-  # TODO: replace with real venues data.
-  #       num_upcoming_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
+  # replace with real venues data.
+  # num_upcoming_shows should be aggregated based on number of upcoming shows per venue.
+  ciudades = Venue.query.with_entities(Venue.city, Venue.state).distinct().all()
+  data = []
+  for ciudad in ciudades:
+    venues = Venue.query.filter_by(city=ciudad[0], state=ciudad[1]).all()
+    data.append({'city': ciudad[0], 'state': ciudad[1], 'venues': venues})
+
   return render_template('pages/venues.html', areas=data);
 
 @app.route('/venues/search', methods=['POST'])
