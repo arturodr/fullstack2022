fce1dc228bbc5775bd825e67ef5b0616619d7d0b
R2 <arturodr@gmail.com>
2022-02-06 12:51:23 -0500

    CORS config

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -15,12 +15,23 @@ def create_app(test_config=None):
     setup_db(app)
 
     '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+    Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    '''
+    CORS(app, resources={r"/*": {"origins": '*'}})
 
     '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+    Use the after_request decorator to set Access-Control-Allow
+    '''
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type, Authorization"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Headers", "GET, POST, PATCH, DELETE, OPTION"
+        )
+        return response
 
     '''
   @TODO: 
