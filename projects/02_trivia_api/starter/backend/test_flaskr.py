a19467a9cc2b3ad3145995b5172265322424bf17
R2 <arturodr@gmail.com>
2022-02-07 16:55:45 -0500

    more unittests

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -22,6 +22,13 @@ class TriviaTestCase(unittest.TestCase):
                                                                self.database_name)
         setup_db(self.app, self.database_path)
 
+        self.test_question = {
+            'question': 'Test Question',
+            'answer': 'Test Answer',
+            'difficulty': 1,
+            'category': '1'
+        }
+
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -54,6 +61,34 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
         self.assertTrue(len(data['questions']))
 
+    def test_create_question(self):
+
+        questions_before_insert = Question.total_questions()
+
+        response = self.client().post('/questions', json=self.test_question)
+        data = json.loads(response.data)
+
+        questions_after_insert = Question.total_questions()
+
+        self.assertTrue(questions_after_insert-questions_before_insert == 1)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_create_question_404(self):
+
+        questions_before_insert = Question.total_questions()
+
+        res = self.client().post('/questions', json={})
+        data = json.loads(res.data)
+
+        questions_after_insert = Question.total_questions()
+
+        self.assertTrue(questions_after_insert-questions_before_insert == 0)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['error'], 422)
+
     def test_delete_questions(self):
         res = self.client().delete('/questions/15')
         data = json.loads(res.data)
@@ -61,6 +96,20 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'])
         self.assertEqual(data['deleted'], 15)
+
+    def test_delete_questions_404(self):
+        res = self.client().delete('/questions/')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['error'], 404)
+
+    def test_delete_questions_404_out_off_range(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['error'], 404)
 
     def test_search_questions(self):
         json_data = {
@@ -73,6 +122,16 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['success'])
         self.assertTrue(len(data['questions']))
         self.assertTrue(data['total_questions'])
+
+    def test_search_questions_422(self):
+        json_data = {
+            'searchTerm': ''
+        }
+        res = self.client().post('/questions/search', json=json_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['error'], 422)
 
     def test_get_questions_in_category(self):
         res = self.client().get('/categories/1/questions')
@@ -95,6 +154,13 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['success'])
         self.assertTrue(data['question'])
 
+    def test_play_422(self):
+        json_data = {}
+        res = self.client().post('/quizzes', json=json_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['error'], 422)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
