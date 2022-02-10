68399414d034d1f4875484dba3ba812b4c2f42d1
R2 <arturodr@gmail.com>
2022-02-09 23:13:43 -0500

    delete_drink

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -127,8 +127,10 @@ def patch_drink(drink_id):
     }), 200
 
 
-'''
-@TODO implement endpoint
+@app.route("/drinks/<int:drink_id>", methods=["DELETE"])
+@requires_auth('delete:drinks')
+def delete_drink(drink_id):
+    """
     DELETE /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -137,7 +139,17 @@ def patch_drink(drink_id):
     returns status code 200 and json {"success": True, "delete": id}
         where id is the id of the deleted record
         or appropriate status code indicating reason for failure
-'''
+    """
+    drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+    if not drink:
+        abort(404)
+
+    drink.delete()
+
+    return jsonify({
+        "success": True,
+        "delete": drink_id
+    }), 200
 
 
 # Error Handling
